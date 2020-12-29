35d5cf69b278664fd937f57927372aa5147c42ba
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-28 21:05:31 -0500

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Hung-Chun Tsai on 2020-12-28.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
